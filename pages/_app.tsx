0034dbf9fcc7b55e2409a21b8c6ed81c1ccb3b58
leehj050211 <leehj050211@gmail.com>
2023-03-17 21:10:40 +0900

    ADD: GA 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,27 +9,47 @@ import Alert from '../components/common/overlay/alert'
 import { SettingBox } from '../components/common/settingPopup'
 import { Header } from '../components/common/header'
 import Sidebar from '../components/common/sidebar/sidebar'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <div className='wrap'>
-        <RecoilRoot>
-          <Sidebar />
-          <main className='scroll-bar'>
-            <Component {...pageProps} />
-          </main>
-          <>
-            <Header />
-            <Toast />
-            <Alert />
-            <LoadingDim />
-            <ModalDim />
-            <AccountBox />
-            <SettingBox />
-          </>
-        </RecoilRoot>
-      </div>
-    )
+  return (<>
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+    />
+    <Script
+      id="gtag-init"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+              });
+            `,
+      }}
+    />
+    <div className='wrap'>
+      <RecoilRoot>
+        <Sidebar />
+        <main className='scroll-bar'>
+          <Component {...pageProps} />
+        </main>
+        <>
+          <Header />
+          <Toast />
+          <Alert />
+          <LoadingDim />
+          <ModalDim />
+          <AccountBox />
+          <SettingBox />
+        </>
+      </RecoilRoot>
+    </div>
+  </>
+  )
 }
 
 export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang="kr">
-                <Head />
-                <body className="dark">
-                    <Main />
-                    <NextScript />
-                    <div id="modal-wrap"/>
-                    <div id="overlay-wrap"/>
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html lang="kr">
+        <Head />
+        <body className="dark">
+          <Main />
+          <NextScript />
+          <div id="modal-wrap" />
+          <div id="overlay-wrap" />
+        </body>
+      </Html>
+    );
+  }
 }
